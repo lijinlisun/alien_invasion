fa00e85d09114faab03a0a637270aef8bbad89a8
lijinlisun <1106384732@qq.com>
2021-09-10 23:15:02 +0800

    跟新

--- alien_invasion/alien_invasion.py
+++ alien_invasion/alien_invasion.py
@@ -2,6 +2,7 @@ import pygame
 from settings import Settings
 from ship import Ship
 import game_function as gf
+from pygame.sprite import Group
 
 
 def run_game():
@@ -11,14 +12,22 @@ def run_game():
     screen = pygame.display.set_mode((ai_settings.screen_width, ai_settings.screen_height))
     pygame.display.set_caption("Alien Invasion")
     ship = Ship(ai_settings, screen)
+    # 创建一个用于存储子弹的编组
+    bullets = Group()
     # 设置背景颜色
     bg_color = (230, 230, 230)
     # 开始游戏的主循环
     while True:
 
-        gf.check_events(ship)
+        gf.check_events(ai_settings, screen, ship, bullets)
         ship.update()
-        gf.update_screen(ai_settings, screen, ship)
+        bullets.update()
+
+        # 删除已消失的子弹
+        for bullet in bullets.copy():
+            if bullet.rect.bottom <= 0:
+                bullets.remove()
+        gf.update_screen(ai_settings, screen, ship, bullets)
 
 
 
